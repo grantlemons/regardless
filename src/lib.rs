6654a3ee8a8f509b18a813ae8233c431f4d66aa0
Grant Lemons <grantlemons@aol.com>
2025-03-04 16:03:54 -0700

    first draft, had to compromise with WrappableError trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,101 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::{
+    error::Error as StdError,
+    fmt::Display,
+    ops::{Deref, DerefMut},
+};
+
+pub type Result<T, E = Error> = std::result::Result<T, E>;
+pub trait WrappableError: StdError + Send + Sync + 'static {}
+
+#[derive(Debug)]
+pub struct Error {
+    inner: Box<dyn StdError + Send + Sync + 'static>,
+    context: Vec<String>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Deref for Error {
+    type Target = dyn StdError + Send + Sync + 'static;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn deref(&self) -> &Self::Target {
+        self.inner.as_ref()
+    }
+}
+
+impl DerefMut for Error {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.inner.as_mut()
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}\n{}",
+            self.inner,
+            self.context
+                .iter()
+                .fold(String::new(), |acc, c| acc + "\n" + c)
+        )
+    }
+}
+impl StdError for Error {}
+
+impl<E: WrappableError> From<E> for Error {
+    fn from(value: E) -> Self {
+        Self {
+            inner: Box::new(value),
+            context: Vec::new(),
+        }
+    }
+}
+
+impl Error {
+    pub fn extend_context(&mut self, s: String) {
+        self.context.push(s)
+    }
+}
+
+pub trait Context<T, E> {
+    fn context<C>(self, context: C) -> Result<T, Error>
+    where
+        C: Display + Send + Sync + 'static;
+    fn with_context<C, F>(self, f: F) -> Result<T, Error>
+    where
+        C: Display + Send + Sync + 'static,
+        F: FnOnce() -> C;
+}
+
+impl<T, E> Context<T, E> for Result<T, E>
+where
+    E: WrappableError,
+{
+    fn context<C>(self, context: C) -> Result<T, Error>
+    where
+        C: Display + Send + Sync + 'static,
+    {
+        match self {
+            Ok(ok) => Ok(ok),
+            Err(error) => Err({
+                let mut res = Error::from(error);
+                res.extend_context(context.to_string());
+                res
+            }),
+        }
+    }
+
+    fn with_context<C, F>(self, context: F) -> Result<T, Error>
+    where
+        C: Display + Send + Sync + 'static,
+        F: FnOnce() -> C,
+    {
+        match self {
+            Ok(ok) => Ok(ok),
+            Err(error) => Err({
+                let mut res = Error::from(error);
+                res.extend_context(context().to_string());
+                res
+            }),
+        }
     }
 }
